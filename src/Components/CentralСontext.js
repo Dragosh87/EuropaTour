933f0cd8cd0251f5c242ae49303043055b374948
Albert Tofan <tofan.albert99@gmail.com>
2023-07-05 19:51:57 +0300

    Hey

--- src/Components/CentralСontext.js
+++ src/Components/CentralСontext.js
@@ -6,6 +6,7 @@ function CentralContext() {
     <div className="CentralContext" style={{ flex: '0 0 60%', backgroundColor: 'lightpink' }}>
       <p>CentralContext</p>
       <p><i>VERIFICAT</i></p>
+      <p><i>Hey</i></p>
     </div>
   );
 }
