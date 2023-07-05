de29d21245ed1c42aba22fb9cdb07f6d6ef8cd8b
Andreea Manole <andreeamanole59@gmail.com>
2023-07-05 19:20:51 +0300

    Adaugare nume la component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,6 +5,7 @@ function Navbar() {
   return (
     <div className="Navbar" style={{ flex: '0 0 30%', backgroundColor: 'lightblue' }}>
       <p1>Navbar</p1>
+      <p>Andreea</p>
     </div>
   );
 }
