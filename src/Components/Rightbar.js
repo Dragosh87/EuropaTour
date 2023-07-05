a2e0e102afb2a738e89a2e8aad50b5844750eb81
Dragosh87 <marcinschi87@gmail.com>
2023-07-05 21:56:54 +0300

    Adaugat bara de selectare tara

--- src/Components/Rightbar.js
+++ src/Components/Rightbar.js
@@ -1,17 +1,98 @@
-import React from 'react';
-import './Rightbar.css';
+// import React from 'react';
+// import './Rightbar.css';
 
-function Rightbar() {
-  return (
-    <div className="Rightbar" style={{ flex: '0 0 20%', backgroundColor: 'lightyellow' }}>
-      <p>Rightbar</p>
-      <p>Salut</p>
-    </div>
+// function Rightbar() {
+//   return (
+//     <div className="Rightbar" style={{ flex: '0 0 20%', backgroundColor: 'lightyellow' }}>
+//       <p>Rightbar</p>
+//       <p>Salut</p>
+//     </div>
     
 
 
+//   );
+// }
+
+// export default Rightbar;
+import React, { useState, useEffect } from 'react';
+import './Rightbar.css';
+
+const API_EUR = 'https://restcountries.com/v3.1/region/europe';
+
+function Rightbar() {
+  const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const [showDetails, setShowDetails] = useState(false);
+  const [showGeolocation, setShowGeolocation] = useState(false);
+
+  useEffect(() => {
+    fetch(API_EUR)
+      .then((response) => response.json())
+      .then((data) => setCountries(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleCountrySelect = (country) => {
+    setSelectedCountry(country);
+    setShowDetails(false);
+    setShowGeolocation(false);
+
+    setTimeout(() => {
+      setShowDetails(true);
+
+      setTimeout(() => {
+        setShowGeolocation(true);
+      }, 500); 
+    }, 500); 
+  };
+
+  const handleOpenGoogleMaps = () => {
+    if (selectedCountry && selectedCountry.maps.googleMaps) {
+      window.open(selectedCountry.maps.googleMaps, '_blank');
+    }
+  };
+
+  return (
+    <div className="Rightbar" style={{ flex: '0 0 20%', backgroundColor: 'lightyellow' }}>
+      <h3>Explorează țările Europei și descoperă informații interesante</h3>
+
+      <div className="ChooseCountry">
+        <select
+          value={selectedCountry ? selectedCountry.name.common : ''}
+          onChange={(e) => {
+            const selectedCountry = countries.find(
+              (country) => country.name.common === e.target.value
+            );
+            handleCountrySelect(selectedCountry);
+          }}
+        >
+          <option value="">Selectează țara</option>
+          {countries.map((country, index) => (
+            <option key={index} value={country.name.common}>
+              {country.name.common}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {selectedCountry && showDetails && (
+        <div className="CountryDetails">
+          <h4>Steagul țării</h4>
+          <img src={selectedCountry.flags.svg} alt={selectedCountry.name.common} />
+          <h4>Stema</h4>
+          <img src={selectedCountry.coatOfArms.svg} alt="Coat of Arms" />
+          <h4>Capitala</h4>
+          <p>{selectedCountry.capital[0]}</p>
+          <h4>Populație</h4>
+          <p>{selectedCountry.population}</p>
+
+          {showGeolocation && (
+            <button onClick={handleOpenGoogleMaps}>Open Google Maps</button>
+          )}
+        </div>
+      )}
+    </div>
   );
 }
 
 export default Rightbar;
-
