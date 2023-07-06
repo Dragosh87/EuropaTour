ce8a99b7324523ef0705aebbe353b9f4d17f48d1
Andreea Manole <andreeamanole59@gmail.com>
2023-07-06 22:32:14 +0300

    Stilizare options of destinations

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,8 @@
-import React, { useState } from 'react';
+
 import './Navbar.css';
 
 function Navbar() {
-const [open, setOpen] = useState(false);
 
-const handleOpen = () => {
-  setOpen(!open);
-};
 
   return (
     <div className="Navbar" style={{ flex: '0 0 30%', backgroundColor: 'white' }}>
@@ -15,9 +11,29 @@ const handleOpen = () => {
     <li className="item">
       DESTINATIONS
       <ul className="dropdown">
-        <li className="dropdown_item">Option 1</li>
-        <li className="dropdown_item">Option 2</li>
-        <li className="dropdown_item">Option 3</li>
+        <li className="dropdown_item">Albania</li>
+        <li className="dropdown_item">Andorra</li>
+        <li className="dropdown_item">Austria</li>
+        <li className="dropdown_item">Belarus</li>
+        <li className="dropdown_item">Belgium</li>
+        <li className="dropdown_item">Bosnia and Herzegovina</li>
+        <li className="dropdown_item">Croatia</li>
+        <li className="dropdown_item">Cyprus</li>
+        <li className="dropdown_item">Czechia</li>
+        <li className="dropdown_item">Denmark</li>
+        <li className="dropdown_item">Estonia</li>
+        <li className="dropdown_item">Faroe Islands</li>
+        <li className="dropdown_item">France</li>
+        <li className="dropdown_item">Germany</li>
+        <li className="dropdown_item">Gibraltar</li>
+        <li className="dropdown_item">Greece</li>
+        <li className="dropdown_item">Guernsay</li>
+        <li className="dropdown_item">Hungary</li>
+        <li className="dropdown_item">Iceland</li>
+        <li className="dropdown_item">Irland</li>
+        <li className="dropdown_item">Italy</li>
+        <li className="dropdown_item">Isle of Man</li>
+        <li className="dropdown_item">Jersey</li>
       </ul>
     </li>
     <li className="item"><a href="#">ABOUT US</a></li>
