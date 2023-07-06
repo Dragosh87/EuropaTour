b055ee3c26b0dbc1eaec578c0a3cdf36ed34c964
Albert Tofan <tofan.albert99@gmail.com>
2023-07-06 21:53:05 +0300

    ,

--- src/Components/CentralСontext.jsx
+++ src/Components/CentralСontext.jsx
@@ -7,9 +7,9 @@ function CentralContext() {
       <section className="CentralContext">
       <img className='image' src='https://www.tosomeplacenew.com/wp-content/uploads/2021/12/Getting-ready-for-Europe-1024x819.jpg'/>
       <ul className='paragraf'>
-        <li>Toată lumea visează să călătorească în Europa și există un motiv pentru care.
+        <li>Toată lumea visează să călătorească în Europa și există un motiv pentru asta.
            Fie că faci prima ta excursie solo, faci rucsacul cu un buget redus, cauti o evadare elegantă sau vrei să 
-           explorezi noi orașe și culturi (nu uita de mâncare!), Europa are ceva pentru toată lumea.
+           explorezi noi orașe și culturi, Europa are ceva pentru toată lumea.
         </li>
         <li>
         Sezonul turistic de vârf al Europei este vara! Din iunie până în august, călătorii din întreaga
@@ -19,13 +19,13 @@ function CentralContext() {
       </section>
       <ul className='paragraf2'>
         <li>
-        Cu toate acestea, dacă mergi cu rucsac prin Europa, cel mai bun moment pentru a călători în regiune 
+        Cu toate acestea, dacă mergi cu rucsacul prin Europa, cel mai bun moment pentru a călători în regiune 
           este în extrasezon, din octombrie până în aprilie. Veți avea mai mult noroc să găsiți zboruri ieftine 
           și hotelurile își reduc prețurile.
         </li>
         <li>
         Dar, în funcție de destinație, iarna poate fi o perioadă aglomerată a anului pentru cei care plănuiesc 
-          o vacanță la schi sau care doresc să experimenteze Piețele europene de Crăciun.
+          o vacanță la schi sau care doresc să experimenteze piețele europene de Crăciun.
         </li>
       </ul>
     </div>
