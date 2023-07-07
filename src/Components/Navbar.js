83ff421ab8b0f52cc46e378f217be6805e2341c3
Andreea Manole <andreeamanole59@gmail.com>
2023-07-07 15:50:39 +0300

    Adaugare stilizare si dropdown la Contact element

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,9 +3,6 @@ import './Navbar.css';
 
 function Navbar() {
 
-  const handleOnClick = () => {
-    alert("Send us an email at the adress: eurotravelguide2023@gmail.com or call us: 07XX-XXX-XXX");
-  }
 
   return (
     <div className="Navbar" style={{ flex: '0 0 30%', backgroundColor: 'white' }}>
@@ -19,13 +16,11 @@ function Navbar() {
         <li className="dropdown_item">Austria</li>
         <li className="dropdown_item">Belarus</li>
         <li className="dropdown_item">Belgium</li>
-        <li className="dropdown_item">Bosnia and Herzegovina</li>
         <li className="dropdown_item">Croatia</li>
         <li className="dropdown_item">Cyprus</li>
         <li className="dropdown_item">Czechia</li>
         <li className="dropdown_item">Denmark</li>
         <li className="dropdown_item">Estonia</li>
-        <li className="dropdown_item">Faroe Islands</li>
         <li className="dropdown_item">France</li>
         <li className="dropdown_item">Germany</li>
         <li className="dropdown_item">Gibraltar</li>
@@ -35,12 +30,18 @@ function Navbar() {
         <li className="dropdown_item">Iceland</li>
         <li className="dropdown_item">Irland</li>
         <li className="dropdown_item">Italy</li>
-        <li className="dropdown_item">Isle of Man</li>
         <li className="dropdown_item">Jersey</li>
       </ul>
     </li>
     <li className="item"><a href="#">ABOUT US</a></li>
-    <li className="item"><a href="#" onClick={handleOnClick}>CONTACT</a></li>
+    <li className="item1">
+      <a href='mailto: eurotravelguide2023@gmail.com'>
+         CONTACT</a>
+      <ul className="dropdown1">
+          <li className="dropdown_item1">Email: eurotravelguide2023@gmail.com</li>
+          <li className="dropdown_item1">Contact: 0232.080.332</li>
+      </ul>
+      </li>
   </ul>
 </nav>
 
