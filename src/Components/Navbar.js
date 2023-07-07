2713c3bc2be69d984680a34502f4e0530328b8bc
Andreea Manole <andreeamanole59@gmail.com>
2023-07-07 16:18:58 +0300

    Adaugare logo la title

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -39,15 +39,16 @@ function Navbar() {
          CONTACT</a>
       <ul className="dropdown1">
           <li className="dropdown_item1">Email: eurotravelguide2023@gmail.com</li>
-          <li className="dropdown_item1">Contact: 0232.080.332</li>
+          <li className="dropdown_item1">Contact: 0232 080 332</li>
       </ul>
       </li>
   </ul>
 </nav>
-
+    <div className="title_of_page">
+      <img src="https://media.istockphoto.com/id/1199315910/vector/europe-union-vector-star-icon-european-union-flag-symbol-euro-icon-vector.jpg?s=612x612&w=0&k=20&c=z7wwiYq3XAwHedcqGJfcWZWk3mXJpKeacRjtCNhw13Q=" alt="logoEuropa" width="200" height="200"></img>
       <h1 className='header'>Europe Travel Guide</h1>
+      </div>
       <hr />
-
     </div>
   );
 }
