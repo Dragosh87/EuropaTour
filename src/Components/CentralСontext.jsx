9d4e7787084b77394614c3ee439778586a18c695
Albert Tofan <tofan.albert99@gmail.com>
2023-07-12 14:39:36 +0300

    adaugare text

--- src/Components/CentralСontext.jsx
+++ src/Components/CentralСontext.jsx
@@ -30,11 +30,113 @@ function CentralContext() {
       </ul>
       <div className='paragraf3'>
       <section className='titlu1'>
-      <h2>WHAT TO EXPECT</h2>
+      <h2><b>WHAT TO EXPECT</b></h2>
       </section>
       <section className='image2'>
       <img className='img2' src='https://img.freepik.com/free-photo/view-travel-items-assortment-still-life_23-2149617645.jpg'/>
       </section>
+      </div>
+      <div className='est'>
+      <section className='europEst'>
+      <p>
+      <b>Eastern Europe</b>: Eastern Europe generally refers to the countries that were behind the Iron Curtain. It’s made up of Belarus,
+       Bulgaria, Czech Republic, Hungary, Poland, Moldova, Romania, Russia, Slovakia, and Ukraine. It’s a cheaper destination for 
+       budget travelers, and some of its top historical sites include the Prague Castle, the State Hermitage Museum and Word War II history.
+      </p>
+      </section>
+      <section className='arrowEst'>
+      <svg width="388" height="395" viewBox="0 0 388 395" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d_2_21)">
+<path d="M367.562 385.427C367.798 386.221 368.633 386.674 369.427 386.438L382.369 382.596C383.163 382.36 383.615 381.525 383.38 380.731C383.144 379.937 382.309 379.484 381.515 379.72L370.011 383.135L366.596 371.631C366.36 370.837 365.525 370.385 364.731 370.62C363.937 370.856 363.484 371.691 363.72 372.485L367.562 385.427ZM176 218L177.263 218.808L176 218ZM4.00003 11.0006C4.00003 15.4189 7.58175 19.0006 12 19.0006C16.4183 19.0006 20 15.4189 20 11.0006C20 6.58234 16.4183 3.00062 12 3.00062C7.58175 3.00062 4.00003 6.58234 4.00003 11.0006ZM370.319 385.715C381.807 364.525 383.637 330.294 378.047 294.384C372.446 258.407 359.344 220.4 340.597 191.49C321.885 162.634 297.298 142.492 268.662 143.095C240.036 143.698 208.126 165.019 174.737 217.191L177.263 218.808C210.492 166.889 241.641 146.664 268.725 146.094C295.798 145.524 319.572 164.581 338.08 193.122C356.554 221.611 369.533 259.2 375.082 294.846C380.642 330.559 378.693 363.975 367.681 384.285L370.319 385.715ZM174.737 217.191C171.788 221.798 169.121 224.58 166.739 225.956C164.45 227.278 162.424 227.313 160.452 226.372C158.368 225.378 156.205 223.221 153.977 219.838C151.766 216.48 149.585 212.059 147.403 206.743C138.643 185.395 130.292 150.714 119.812 115.571C109.382 80.5936 96.8491 45.2087 79.7346 23.0216C71.1613 11.9073 61.3246 3.95278 49.8807 1.13C38.3946 -1.70319 25.5724 0.701257 11.202 9.73053L12.7981 12.2707C26.6777 3.54983 38.6679 1.45414 49.1623 4.0427C59.6989 6.64168 69.0106 14.0308 77.3592 24.8539C94.0884 46.5416 106.493 81.4066 116.938 116.429C127.333 151.286 135.794 186.355 144.628 207.882C146.845 213.284 149.111 217.902 151.472 221.488C153.816 225.048 156.349 227.739 159.161 229.08C162.086 230.476 165.156 230.335 168.24 228.553C171.231 226.826 174.212 223.576 177.263 218.808L174.737 217.191Z" fill="#BF9270"/>
+</g>
+<defs>
+<filter id="filter0_d_2_21" x="3.05176e-05" y="0.00665283" width="387.442" height="394.494" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2_21"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2_21" result="shape"/>
+</filter>
+</defs>
+</svg>
+      </section>
+      </div>
+      <div className='nord'>
+        <section className='arrowNord'>
+        <svg width="287" height="305" viewBox="0 0 287 305" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d_8_2)">
+<path d="M16.7207 296.782C17.4285 297.212 18.3513 296.987 18.7817 296.279L25.7954 284.744C26.2258 284.036 26.0009 283.114 25.2931 282.683C24.5852 282.253 23.6625 282.478 23.2321 283.186L16.9976 293.439L6.74428 287.205C6.03643 286.774 5.1137 286.999 4.6833 287.707C4.2529 288.415 4.47782 289.338 5.18566 289.768L16.7207 296.782ZM148 170L146.952 171.073L146.952 171.073L148 170ZM266.5 8.00015C266.5 12.4184 270.082 16.0002 274.5 16.0002C278.918 16.0002 282.5 12.4184 282.5 8.00015C282.5 3.58187 278.918 0.000150681 274.5 0.000150681C270.082 0.000150681 266.5 3.58187 266.5 8.00015ZM18.9573 295.145C9.31573 255.589 20.5898 203.931 44.7512 172.54C56.8112 156.871 71.9527 146.412 89.1446 144.838C106.312 143.265 125.902 150.519 146.952 171.073L149.048 168.927C127.597 147.981 107.188 140.173 88.871 141.85C70.5785 143.525 54.7513 154.629 42.3738 170.71C17.6603 202.819 6.18429 255.411 16.0427 295.855L18.9573 295.145ZM146.952 171.073C154.943 178.876 161.699 184.872 167.388 189.257C173.06 193.63 177.745 196.458 181.59 197.853C185.43 199.245 188.702 199.295 191.246 197.652C193.74 196.042 195.101 193.074 195.809 189.479C197.224 182.291 196.278 171.219 194.515 158.217C193.628 151.677 192.523 144.583 191.377 137.165C190.23 129.742 189.041 121.989 187.981 114.107C185.86 98.3367 184.263 82.1018 184.554 67.0615C184.846 52.0045 187.03 38.2716 192.374 27.435C197.689 16.6604 206.138 8.71764 219.135 5.1481C232.207 1.55784 250.023 2.35348 274.076 9.43902L274.924 6.56128C250.602 -0.603227 232.168 -1.54244 218.34 2.25523C204.438 6.07362 195.339 14.6415 189.684 26.108C184.059 37.5125 181.85 51.7511 181.555 67.0034C181.259 82.2725 182.88 98.6851 185.008 114.507C186.072 122.422 187.266 130.201 188.412 137.624C189.56 145.051 190.66 152.114 191.542 158.62C193.318 171.71 194.169 182.282 192.866 188.9C192.214 192.208 191.088 194.183 189.619 195.132C188.2 196.048 186.033 196.273 182.612 195.032C179.196 193.794 174.798 191.182 169.219 186.881C163.657 182.593 156.992 176.685 149.048 168.927L146.952 171.073Z" fill="#BF9270"/>
+</g>
+<defs>
+<filter id="filter0_d_8_2" x="0.464752" y="0.00012207" width="286.035" height="305" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_8_2"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_8_2" result="shape"/>
+</filter>
+</defs>
+</svg>
+        </section>
+        <section className='europNord'>
+        <b>Northern Europe</b>: Northern Europe is known for its stunning fjords, wintery landscapes, and the most beautiful natural phenomena, the Northern Lights. If you want to see 
+        them, you’ll need to visit in the winter when conditions are at their best. Northern Europe encapsulates Scandanavia, the Baltics, and the United Kingdom.
+        </section>
+      </div>
+      <div className='sud'>
+        <section className='europSud'>
+        <b>Southern Europe</b>: Southern Europe is made up of all the countries close to the Mediterranean Sea. Spain, Italy, Malta, Greece, Croatia, Bosnia and Herzegovina, and Montenegro
+         are some of the most visited destinations in the region. It’s known for its incredible sailing, natural wonders, and ancient history. And the food! Talk about a wine lover’s dream destination.
+        </section>
+        <section className='arrowSud'>
+        <svg width="388" height="395" viewBox="0 0 388 395" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d_2_21)">
+<path d="M367.562 385.427C367.798 386.221 368.633 386.674 369.427 386.438L382.369 382.596C383.163 382.36 383.615 381.525 383.38 380.731C383.144 379.937 382.309 379.484 381.515 379.72L370.011 383.135L366.596 371.631C366.36 370.837 365.525 370.385 364.731 370.62C363.937 370.856 363.484 371.691 363.72 372.485L367.562 385.427ZM176 218L177.263 218.808L176 218ZM4.00003 11.0006C4.00003 15.4189 7.58175 19.0006 12 19.0006C16.4183 19.0006 20 15.4189 20 11.0006C20 6.58234 16.4183 3.00062 12 3.00062C7.58175 3.00062 4.00003 6.58234 4.00003 11.0006ZM370.319 385.715C381.807 364.525 383.637 330.294 378.047 294.384C372.446 258.407 359.344 220.4 340.597 191.49C321.885 162.634 297.298 142.492 268.662 143.095C240.036 143.698 208.126 165.019 174.737 217.191L177.263 218.808C210.492 166.889 241.641 146.664 268.725 146.094C295.798 145.524 319.572 164.581 338.08 193.122C356.554 221.611 369.533 259.2 375.082 294.846C380.642 330.559 378.693 363.975 367.681 384.285L370.319 385.715ZM174.737 217.191C171.788 221.798 169.121 224.58 166.739 225.956C164.45 227.278 162.424 227.313 160.452 226.372C158.368 225.378 156.205 223.221 153.977 219.838C151.766 216.48 149.585 212.059 147.403 206.743C138.643 185.395 130.292 150.714 119.812 115.571C109.382 80.5936 96.8491 45.2087 79.7346 23.0216C71.1613 11.9073 61.3246 3.95278 49.8807 1.13C38.3946 -1.70319 25.5724 0.701257 11.202 9.73053L12.7981 12.2707C26.6777 3.54983 38.6679 1.45414 49.1623 4.0427C59.6989 6.64168 69.0106 14.0308 77.3592 24.8539C94.0884 46.5416 106.493 81.4066 116.938 116.429C127.333 151.286 135.794 186.355 144.628 207.882C146.845 213.284 149.111 217.902 151.472 221.488C153.816 225.048 156.349 227.739 159.161 229.08C162.086 230.476 165.156 230.335 168.24 228.553C171.231 226.826 174.212 223.576 177.263 218.808L174.737 217.191Z" fill="#BF9270"/>
+</g>
+<defs>
+<filter id="filter0_d_2_21" x="3.05176e-05" y="0.00665283" width="387.442" height="394.494" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2_21"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2_21" result="shape"/>
+</filter>
+</defs>
+</svg>
+        </section>
+      </div>
+      <div className='west'>
+        <section className='arrowWest'>
+        <svg width="287" height="305" viewBox="0 0 287 305" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d_8_2)">
+<path d="M16.7207 296.782C17.4285 297.212 18.3513 296.987 18.7817 296.279L25.7954 284.744C26.2258 284.036 26.0009 283.114 25.2931 282.683C24.5852 282.253 23.6625 282.478 23.2321 283.186L16.9976 293.439L6.74428 287.205C6.03643 286.774 5.1137 286.999 4.6833 287.707C4.2529 288.415 4.47782 289.338 5.18566 289.768L16.7207 296.782ZM148 170L146.952 171.073L146.952 171.073L148 170ZM266.5 8.00015C266.5 12.4184 270.082 16.0002 274.5 16.0002C278.918 16.0002 282.5 12.4184 282.5 8.00015C282.5 3.58187 278.918 0.000150681 274.5 0.000150681C270.082 0.000150681 266.5 3.58187 266.5 8.00015ZM18.9573 295.145C9.31573 255.589 20.5898 203.931 44.7512 172.54C56.8112 156.871 71.9527 146.412 89.1446 144.838C106.312 143.265 125.902 150.519 146.952 171.073L149.048 168.927C127.597 147.981 107.188 140.173 88.871 141.85C70.5785 143.525 54.7513 154.629 42.3738 170.71C17.6603 202.819 6.18429 255.411 16.0427 295.855L18.9573 295.145ZM146.952 171.073C154.943 178.876 161.699 184.872 167.388 189.257C173.06 193.63 177.745 196.458 181.59 197.853C185.43 199.245 188.702 199.295 191.246 197.652C193.74 196.042 195.101 193.074 195.809 189.479C197.224 182.291 196.278 171.219 194.515 158.217C193.628 151.677 192.523 144.583 191.377 137.165C190.23 129.742 189.041 121.989 187.981 114.107C185.86 98.3367 184.263 82.1018 184.554 67.0615C184.846 52.0045 187.03 38.2716 192.374 27.435C197.689 16.6604 206.138 8.71764 219.135 5.1481C232.207 1.55784 250.023 2.35348 274.076 9.43902L274.924 6.56128C250.602 -0.603227 232.168 -1.54244 218.34 2.25523C204.438 6.07362 195.339 14.6415 189.684 26.108C184.059 37.5125 181.85 51.7511 181.555 67.0034C181.259 82.2725 182.88 98.6851 185.008 114.507C186.072 122.422 187.266 130.201 188.412 137.624C189.56 145.051 190.66 152.114 191.542 158.62C193.318 171.71 194.169 182.282 192.866 188.9C192.214 192.208 191.088 194.183 189.619 195.132C188.2 196.048 186.033 196.273 182.612 195.032C179.196 193.794 174.798 191.182 169.219 186.881C163.657 182.593 156.992 176.685 149.048 168.927L146.952 171.073Z" fill="#BF9270"/>
+</g>
+<defs>
+<filter id="filter0_d_8_2" x="0.464752" y="0.00012207" width="286.035" height="305" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_8_2"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_8_2" result="shape"/>
+</filter>
+</defs>
+</svg>
+        </section>
+        <section className='europWest'>
+        <b>Western Europe</b>: Western Europe is one of the most popular regions in Europe. It’s home to countries like Austria, France, Germany, Netherlands, and Switzerland that attract millions of visitors each year. Some of its most-traveled cities are Paris, Berlin, Vienna and Amsterdam!
+        </section>
       </div>
       {/* <footer className='footer'>
         <img className='imgFooter' src='https://theblondeabroad.com/wp-content/uploads/2018/07/Footer-Manifesto.png'/>
