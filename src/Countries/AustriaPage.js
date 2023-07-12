c038849d05351cc1788962f9907d2e864ca33e09
Dragosh87 <marcinschi87@gmail.com>
2023-07-12 20:07:17 +0300

    mici schimbari navbar

---
+++ src/Countries/AustriaPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AustriaPage() {
+  return (
+    <div>
+      <h2>Informații despre Austria</h2>
+      {/* Adăugați conținutul specific paginii Albania aici */}
+    </div>
+  );
+}
+
+export default AustriaPage;
