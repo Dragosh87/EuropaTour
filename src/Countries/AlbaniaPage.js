958c9600ee928c855a3eea300c7618234e185c5e
Andreea Manole <andreeamanole59@gmail.com>
2023-07-14 17:15:46 +0300

    Stilizare AlbaniaPage

--- src/Countries/AlbaniaPage.js
+++ src/Countries/AlbaniaPage.js
@@ -11,7 +11,7 @@ function AlbaniaPage() {
       </div>
       <div className='content_of_cities'>
         <img className='imageHeader' src='https://www.danflyingsolo.com/wp-content/uploads/2017/04/AlbaniaPhotos2-3.jpg' alt='albaniaPhoto'></img>
-        <h3>Pick a destination</h3>
+        <h3 className='title_of_list'>Pick a destination</h3>
         <div className='first_part'>
           <svg width="318" height="32" viewBox="0 0 318 32" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g filter="url(#filter0_d_3_5)">
@@ -82,34 +82,30 @@ function AlbaniaPage() {
       <p className='paragraf'>April to October is the best time to visit Albania!</p>
       </div>
            
-            {/* <div className='money'>
-              <h1 className='dollar'>$</h1>
-              <h4>Currency</h4>
-              <p className='paragraf1'>The currency in Albania is Lekë. 1 USD is equivalent to 115 LEK!</p>
-            </div> */}
+           
             </section>
         <h2 className='header2'>Here are some key details about the country:</h2>
                 <div className='info'>
-                <p className='info_item'>Location and Geography: Albania is a country located in Southeastern Europe, bordered by Montenegro to the northwest, Kosovo to the northeast, North Macedonia to the east, and Greece to the south. It has a coastline along the Adriatic and Ionian Seas. Albania's landscape is diverse, with the Albanian Alps in the north, fertile plains in the central region, and beautiful beaches along the coast.</p>
+                <p className='info_item'><span style={{fontWeight: 'bold'}}>1.Location and Geography:</span> Albania is a country located in Southeastern Europe, bordered by Montenegro to the northwest, Kosovo to the northeast, North Macedonia to the east, and Greece to the south. It has a coastline along the Adriatic and Ionian Seas. Albania's landscape is diverse, with the Albanian Alps in the north, fertile plains in the central region, and beautiful beaches along the coast.</p>
                 <img className='landscapes' src='https://media.istockphoto.com/id/525380299/photo/tirane-pinned-on-a-map-of-europe.jpg?s=612x612&w=0&k=20&c=oI5VItTdglURMKY0QiCMFTF6JjPjmCFEX7OBSRIjA3Y='></img> 
                 <img className='landscapes' src='https://travelplanner.ro/blog/wp-content/uploads/2023/05/albania-2.jpg'></img>
-            <p className='info_item'>Language: The official language of Albania is Albanian. It is spoken by the majority of the population. English is also widely spoken, especially among the younger generation and in tourist areas.</p>
-            <p className='info_item'>Population: As of my knowledge cutoff in September 2021, the estimated population of Albania was around 2.8 million people.</p>
+            <p className='info_item'><span style={{fontWeight: 'bold'}}>2.Language:</span> The official language of Albania is Albanian. It is spoken by the majority of the population. English is also widely spoken, especially among the younger generation and in tourist areas.</p>
+            <p className='info_item'><span style={{fontWeight: 'bold'}}>3.Population:</span> As of my knowledge cutoff in September 2021, the estimated population of Albania was around 2.8 million people.</p>
                <img className='landscapes' src='https://www.national-geographic.pl/media/cache/big/uploads/media/default/0014/64/albania-dlugo-byla-zamknieta-na-swiat-teraz-podbija-turystyczne-serca-na-czym-polega-fenomen-kraju.jpeg'></img>
                <img className='landscapes' src='https://www.shutterstock.com/shutterstock/photos/1972254578/display_1500/stock-photo-amazing-spring-view-of-old-mes-bridge-stunning-morning-landscape-of-shkoder-picturesque-outdoor-1972254578.jpg'></img>
-            <p className='info_item'>The capital and largest city of Albania is Tirana. Other major cities include Durres, Vlora, Shkodra, and Elbasan.</p>
-            <p className='info_item'>History: Albania has a rich and complex history. It was part of the Roman Empire, Byzantine Empire, and Ottoman Empire. Albania declared its independence in 1912. The country experienced communist rule under Enver Hoxha from 1944 to 1992. After the fall of communism, Albania transitioned to a multiparty democracy.</p>
+            <p className='info_item'><span style={{fontWeight: 'bold'}}>4.The capital and largest city of Albania is Tirana.</span> Other major cities include Durres, Vlora, Shkodra, and Elbasan.</p>
+            <p className='info_item'><span style={{fontWeight: 'bold'}}>5.History:</span> Albania has a rich and complex history. It was part of the Roman Empire, Byzantine Empire, and Ottoman Empire. Albania declared its independence in 1912. The country experienced communist rule under Enver Hoxha from 1944 to 1992. After the fall of communism, Albania transitioned to a multiparty democracy.</p>
                <img className='landscapes' src='https://media.istockphoto.com/id/1068542796/photo/gjirokaster-albania-20-september-2016-view-of-old-town-gjirokaster-from-the-castle-albania.jpg?s=612x612&w=0&k=20&c=8mt6K7xnakbENjHfmL2tWmwrHjoHIaI8yk0-89yuiWU='></img>
                <img className='landscapes' src='https://st4.depositphotos.com/27201292/39426/i/600/depositphotos_394266648-stock-photo-image-shows-view-vjosa-river.jpg'></img>
-            <p className='info_item'>Culture: Albanian culture is a blend of various influences, including Illyrian, Roman, Byzantine, Ottoman, and more. The country has a rich tradition of music, dance, and folklore. Albanians are known for their hospitality and respect for guests.</p>
-            <p className='info_item'>Tourism: Albania has become an increasingly popular tourist destination in recent years. The country offers a diverse range of attractions, including ancient archaeological sites, picturesque towns, stunning beaches, and scenic mountain landscapes. Some notable tourist destinations include the capital Tirana, the UNESCO World Heritage site of Berat, the historic city of Gjirokastër, and the Albanian Riviera.</p>
+            <p className='info_item'><span style={{fontWeight: 'bold'}}>6.Culture:</span> Albanian culture is a blend of various influences, including Illyrian, Roman, Byzantine, Ottoman, and more. The country has a rich tradition of music, dance, and folklore. Albanians are known for their hospitality and respect for guests.</p>
+            <p className='info_item'><span style={{fontWeight: 'bold'}}>7.Tourism:</span> Albania has become an increasingly popular tourist destination in recent years. The country offers a diverse range of attractions, including ancient archaeological sites, picturesque towns, stunning beaches, and scenic mountain landscapes. Some notable tourist destinations include the capital Tirana, the UNESCO World Heritage site of Berat, the historic city of Gjirokastër, and the Albanian Riviera.</p>
                <img className='landscapes' src='https://expatra.com/wp-content/uploads/2021/08/saranda_albania.jpg'></img>
                <img className='landscapes' src='https://resources.stuff.co.nz/content/dam/images/1/q/f/e/6/g/image.related.StuffLandscapeSixteenByNine.710x400.1qc5hn.png/1529441668883.jpg?format=pjpg&optimize=medium'></img>
-            <p className='info_item'>Economy: Albania has been undergoing a process of economic development and reforms since the 1990s. Its economy is primarily based on services, agriculture, and industry. The country has made progress in attracting foreign investment and has seen growth in sectors such as tourism and energy.</p>
-            <p className='info_item'>Cuisine: Albanian cuisine is influenced by Mediterranean and Balkan culinary traditions. Popular dishes include byrek (a savory pastry), tavë kosi (baked lamb with yogurt), qofte (meatballs), and various fresh seafood dishes. Dairy products, olives, and vegetables are also common in Albanian cuisine.</p>
+            <p className='info_item'><span style={{fontWeight: 'bold'}}>8.Economy:</span> Albania has been undergoing a process of economic development and reforms since the 1990s. Its economy is primarily based on services, agriculture, and industry. The country has made progress in attracting foreign investment and has seen growth in sectors such as tourism and energy.</p>
+            <p className='info_item'><span style={{fontWeight: 'bold'}}>9.Cuisine:</span> Albanian cuisine is influenced by Mediterranean and Balkan culinary traditions. Popular dishes include byrek (a savory pastry), tavë kosi (baked lamb with yogurt), qofte (meatballs), and various fresh seafood dishes. Dairy products, olives, and vegetables are also common in Albanian cuisine.</p>
                <img className='landscapes' src='https://albaniatourguide.com/wp-content/uploads/2021/03/permet-7-shutterstock-large.jpg'></img>
                <img className='landscapes' src='https://images.squarespace-cdn.com/content/v1/57322ef32fe131e30c4a3f5d/1513525603271-P26NYXXV3S25QILCY2QJ/ksamilchair.jpg'></img>
-            <p className='info_item'>Nature and Outdoor Activities: Albania is known for its natural beauty. The country offers opportunities for outdoor activities such as hiking, trekking, and camping in its national parks and mountain ranges. The Albanian Alps, Llogara National Park, and Butrint National Park are some popular destinations for nature enthusiasts.</p>
+            <p className='info_item'><span style={{fontWeight: 'bold'}}>10.Nature and Outdoor Activities:</span> Albania is known for its natural beauty. The country offers opportunities for outdoor activities such as hiking, trekking, and camping in its national parks and mountain ranges. The Albanian Alps, Llogara National Park, and Butrint National Park are some popular destinations for nature enthusiasts.</p>
               </div>
           </div>
       </div>
