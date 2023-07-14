9ae0e1751ecbd1ee4d0c0f076d990cee6abcfbfc
Victoria G <v.gustei@gmail.com>
2023-07-15 01:34:34 +0300

    user acount registration WIP

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,42 +1,44 @@
 import "./Navbar.css";
 import React from "react";
+import UserMenu from './userMenu'
 
 function Navbar() {
   return (
-    <div className="Navbar" style={{ flex: '0 0 30%', backgroundColor: 'white' }}>
+    
+    <div className="Navbar">
+      <div className="grid items-center float-right">
+    <UserMenu />
+  </div>
     <nav className="nav_bar">
-  <ul>
-    <li className="item"><a href='/' target="_blank">HOME</a></li>
-    <li className="item">
-      DESTINATIONS
-      <ul className="z-50 dropdown">
-        <li className="dropdown_item"><a href='/albania' target='_blank'>Albania</a></li>
-        <li className="dropdown_item">Austria</li>
-        <li className="dropdown_item">Czech Republic</li>
-        <li className="dropdown_item">France</li>
-        <li className="dropdown_item">Greece</li>
-        <li className="dropdown_item">Iceland</li>
-        <li className="dropdown_item">Italy</li>
-        <li className="dropdown_item">Malta</li>
-        <li className="dropdown_item">Romania</li>
-        <li className="dropdown_item">Spain</li>
+      <ul>
+        <li className="item"><a href='/' target="_blank">HOME</a></li>
+        <li className="item">
+          DESTINATIONS
+          <ul className="z-50 dropdown">
+            <li className="dropdown_item"><a href='/albania' target='_blank'>Albania</a></li>
+            <li className="dropdown_item">Austria</li>
+            <li className="dropdown_item">Czech Republic</li>
+            <li className="dropdown_item">France</li>
+            <li className="dropdown_item">Greece</li>
+            <li className="dropdown_item">Iceland</li>
+            <li className="dropdown_item">Italy</li>
+            <li className="dropdown_item">Malta</li>
+            <li className="dropdown_item">Romania</li>
+            <li className="dropdown_item">Spain</li>
+          </ul>
+        </li>
+        <li className="item"><a href="/tips" target="_blank">TRAVEL TIPS</a></li>
+        <li className="item1">
+          <a href='mailto: eurotravelguide2023@gmail.com'>
+            CONTACT</a>
+          <ul className="dropdown1">
+              <li className="dropdown_item1">Email: eurotravelguide2023@gmail.com</li>
+              <li className="dropdown_item1">Contact: 0232 080 332</li>
+          </ul>
+          </li>
       </ul>
-    </li>
-    <li className="item"><a href="/tips" target="_blank">TRAVEL TIPS</a></li>
-    <li className="item1">
-      <a href='mailto: eurotravelguide2023@gmail.com'>
-         CONTACT</a>
-      <ul className="dropdown1">
-          <li className="dropdown_item1">Email: eurotravelguide2023@gmail.com</li>
-          <li className="dropdown_item1">Contact: 0232 080 332</li>
-      </ul>
-      </li>
-  </ul>
 </nav>
-    <div className="title_of_page">
-      <img className="w-24 h-24" src="https://media.istockphoto.com/id/1199315910/vector/europe-union-vector-star-icon-european-union-flag-symbol-euro-icon-vector.jpg?s=612x612&w=0&k=20&c=z7wwiYq3XAwHedcqGJfcWZWk3mXJpKeacRjtCNhw13Q=" alt="logoEuropa" width="200" height="200"></img>
-      <h1 className='header'>Europe Travel Guide</h1>
-      </div>
+
     </div>
   );
 }
--- src/RegistrationPage.js
+++ src/RegistrationPage.js
@@ -1,41 +1,161 @@
-import React from "react";
 import Navbar from "./Components/Navbar";
+import React, { useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+} from "@material-tailwind/react";
 
 export default function Registration() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const handleOpen = () => setOpen(!open);
+  const [open, setOpen] = useState(false);
+
+  async function handleRegisterUser() {
+    const registrationDetails = {
+      username: username,
+      email: email,
+      password: password,
+    };
+    const response = await fetch(
+      "http://localhost:1337/api/auth/local/register",
+      {
+        method: "POST",
+        cache: "no-cache",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
+        body: JSON.stringify(registrationDetails),
+      }
+    );
+    setOpen(true);
+    return response.json();
+  }
+
   return (
     <>
       <Navbar />
-
+      <Dialog open={open} handler={handleOpen}>
+        <DialogHeader>Account Created Successfully</DialogHeader>
+        <DialogBody divider>
+          Your account was successfully created. Click ok to go to the hompage
+        </DialogBody>
+        <DialogFooter>
+          <Button
+            variant="gradient"
+            color="green"
+            onClick={() => (window.location = "/")}
+          >
+            <span>OK</span>
+          </Button>
+        </DialogFooter>
+      </Dialog>
       <section className="bg-gray-50 dark:bg-gray-900">
-  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-    
-      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Create an account
+                Create an account
               </h1>
-              <form className="space-y-4 md:space-y-6" action="#">
-                  <div>
-                      <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                      <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
-                  </div>
-                  <div>
-                      <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
-                  </div>
-                  <div>
-                      <label for="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                      <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
-                  </div>
-                  <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
-                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Already have an account? <a href="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
-                  </p>
-              </form>
+              <div>
+                <label
+                  for="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label
+                  for="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Username
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder=""
+                  required=""
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label
+                  for="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required=""
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div>
+                <label
+                  for="confirm-password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Confirm password
+                </label>
+                <input
+                  type="password"
+                  name="confirm-password"
+                  id="confirm-password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required=""
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </div>
+              <button
+                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                onClick={handleRegisterUser}
+              >
+                Create an account
+              </button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Already have an account?{" "}
+                <a
+                  href="/login"
+                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >
+                  Login here
+                </a>
+              </p>
+            </div>
           </div>
-      </div>
-  </div>
-</section>
+        </div>
+      </section>
     </>
   );
 }
