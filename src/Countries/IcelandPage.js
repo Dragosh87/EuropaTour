575b1e804381f18d0b02e8141db7586e3be42497
Dragosh87 <marcinschi87@gmail.com>
2023-07-23 16:23:45 +0300

    mici ajustari

--- src/Countries/IcelandPage.js
+++ src/Countries/IcelandPage.js
@@ -45,7 +45,7 @@ export default function IcelandPage() {
             </g>
             <defs>
               <filter id="filter0_d_3_6" x="0.499992" y="0.95406" width="334.5" height="30.0919" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feFlood floodOpacity="0" result="BackgroundImageFix" />
                 <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
                 <feOffset dy="4" />
                 <feGaussianBlur stdDeviation="2" />
