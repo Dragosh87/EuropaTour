f968f250399d78e56d32d809a565f5d7a64799ab
Victoria G <v.gustei@gmail.com>
2023-07-24 13:48:45 +0300

    Error message shown on comments if backend not started

--- src/Components/coments/CommentBox.js
+++ src/Components/coments/CommentBox.js
@@ -10,45 +10,54 @@ export default function CommentBox({
                                    }) {
 
     let [comments, setComments] = useState([]);
+    let [isAvailable, setAvailable] = useState(false);
     const [setItem, getItem] = useContext(LocalContext);
 
-    function fetchComments(){
-        fetch(
-            `http://localhost:1337/api/comment-manager/comments/${pageId}`
-        )
-            .then((response) => response.json())
-            .then((data) => {
-                setComments(data.comments || {})
-            });
+    async function fetchComments(){
+        try {
+            const response = await fetch(`http://localhost:1337/api/comment-manager/comments/${pageId}`);
+            setComments(response.json().comments || {})
+            setAvailable(false)
+        } catch (error) {
+            setAvailable(false)
+        }
     }
 
     useEffect(() => {
         fetchComments()
     }, [pageId]);
 
-    return (
-        <>
-            {
+    const available = (
+        <section className="max-w-2xl px-4 py-10 mx-auto">
+        {
+            (getItem() && getItem().user)
+                ?
+                <PostComment pageId={pageId} onPostComment={fetchComments}/>
+                :
+                <div className='pb-2'><Link to="/login" className='text-red-500'>Log in</Link> to leave a
+                    comment</div>
+        }
+        {
+            comments.length > 0
+                ?
+                comments.map(comment => <Comment key={comment.id} username={comment.author.username}
+                                                 date={comment.updatedAt} content={comment.content}/>)
+                :
+                <div className='italic'>There are no comments yet for this page</div>
+        }
+        </section>
+    )
 
-                <section className="max-w-2xl px-4 py-10 mx-auto">
-                    {
-                        (getItem() && getItem().user)
-                            ?
-                            <PostComment pageId={pageId} onPostComment={fetchComments}/>
-                            :
-                            <div className='pb-2'><Link to="/login" className='text-red-500'>Log in</Link> to leave a
-                                comment</div>}
-                    {
-                        comments.length > 0
-                            ?
-                            comments.map(comment => <Comment key={comment.id} username={comment.author.username}
-                                                             date={comment.updatedAt} content={comment.content}/>)
-                            :
-                            <div className='italic'>There are no comments yet for this page</div>
-                    }
-                </section>
-            }
-        </>
+    const notAvailable = (
+        <section className="max-w-2xl px-4 py-10 mx-auto">
+        {
+            <div className='italic'>Comments not available (start the backend for comments)</div>
+        }
+        </section>)
+
+    return (
+
+        isAvailable ? available : notAvailable
 
     )
 
