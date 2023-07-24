36427025bab598a2792bb9ff45adcb5daf531284
Victoria G <v.gustei@gmail.com>
2023-07-24 14:09:49 +0300

    change colour of comments button

--- src/Components/coments/PostComment.js
+++ src/Components/coments/PostComment.js
@@ -59,7 +59,7 @@ export default function PostComment({pageId, comments, onPostComment}) {
             </div>
             <button
               type="submit"
-              className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
+              className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-[#BF9270]  rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 "
               onClick={handlePost}
               disabled={inProgress}
             >
