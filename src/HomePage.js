a60275b6d1b88180490fbe439a098c8539653822
Albert Tofan <tofan.albert99@gmail.com>
2023-07-26 17:49:44 +0300

    mici modificari

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,70 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Home.css';
 import Navbar from './Components/Navbar';
 import Leftbar from './Components/Leftbar';
 import Rightbar from './Components/Rightbar';
 import CentralContext from './Components/CentralСontext';
 import { ReviewBar } from './Components/barReview';
-import LoginForm from './LoginForm';
-import ModificariForm from './ModificariForm';
 import { Footer } from './Components/footer';
 import Title from "./Components/Title";
 
-
-
 function HomePage() {
+  useEffect(() => {
+    const metaTag = document.createElement('meta');
+    metaTag.name = 'viewport';
+    metaTag.content = 'width=device-width, initial-scale=1.0';
+    document.head.appendChild(metaTag);
+
+    return () => {
+      document.head.removeChild(metaTag);
+    };
+  }, []);
+
   return (
     <>
+      <div className="App" style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+        <Navbar />
+        <Title />
+        <div className="Content" style={{ flex: '1', display: 'flex' }}>
+          <Leftbar />
+          <CentralContext />
+          <Rightbar />
+        </div>
 
-    <div className="App" style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
-      <Navbar />
-     <Title/>
-      <div className="Content" style={{ flex: '1', display: 'flex' }}>
-        <Leftbar />
-        <CentralContext />
-        <Rightbar />
+        <div className='review'>
+          <ReviewBar />
+        </div>
+        <div>
+          <Footer />
+        </div>
       </div>
-
-      <div className='review'>
-        <ReviewBar />
-      </div>
-      <div>
-        <Footer />
-      </div>
-    </div>
     </>
   );
 }
 
 export default HomePage;
-
-// function App() {
-//   return (
-//     <div className="App" style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
-//       {/* Navbar */}
-//       <div className="Navbar" style={{ flex: '0 0 30%', backgroundColor: 'lightblue' }}>
-//         <Navbar />
-//       </div>
-
-//       {/* Content */}
-//       <div className="Content" style={{ flex: '1', display: 'flex' }}>
-//         {/* Leftbar */}
-//         <div className="Leftbar" style={{ flex: '0 0 20%', backgroundColor: 'lightgreen' }}>
-//           <Leftbar />
-//         </div>
-
-//         {/* CentralContent */}
-//         <div className="CentralContent" style={{ flex: '0 0 60%', backgroundColor: 'lightpink' }}>
-//           <CentralContext />
-//         </div>
-
-//         {/* RightBar */}
-//         <div className="Rightbar" style={{ flex: '0 0 20%', backgroundColor: 'lightyellow' }}>
-//           <Rightbar />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
